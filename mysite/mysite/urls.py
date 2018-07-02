3414c005e5011a175ee1ec1e84f8d45aa22fcaf7
Kevin Peng <FateXRebirth@gmail.com>
2018-07-03 05:11:39 +0800

    Template tags

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from trips.views import hello_world
+from trips.views import hello_world, home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello_world),
+    url(r'^$', home),
 ]
--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -7,8 +7,21 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Post
 
 def hello_world(request):
     return render(request, 'hello_world.html', {
         'current_time': str(datetime.now()),
+    })
+
+def home(request):
+    # post1 = Post(title='My First Trip', content='肚子好餓，吃什麼好呢?',  location='台北火車站')
+    # post1.save();
+    # post2 = Post(title='My Second Trip', content='去散散步吧',  location='大安森林公園')
+    # post2.save();
+    # post3 = Post(title='Django 大冒險', content='從靜態到動態',  location='台北市大安區復興南路一段293號')
+    # post3.save();
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
     })
