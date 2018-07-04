91846744566ed5b4ba71592e92c499797535565f
Kevin Peng <FateXRebirth@gmail.com>
2018-07-04 18:22:54 +0800

    Dynamic URL

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from trips.views import hello_world, home
+from trips.views import hello_world, home, post_detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello_world),
     url(r'^$', home),
+    url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
 ]
